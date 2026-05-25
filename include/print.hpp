b3a7910350c2239165bc444456a7060d4dd81282
uswdeco <kathryn.mo.207@gmail.com>
2026-05-25 23:43:11 +0300

    fixed bugs

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,10 @@
-#include <fstream>
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+
 #include <iostream>
 #include <string>
 
-void print(const std::string& text,std::ofstream& out);
-void print(const std::string& text,std::ofstream& out = std::cout);
+void print(const std::string& text,std::ostream& out);
+
+#endif
--- sources/print.cpp
+++ sources/print.cpp
@@ -1,11 +1,6 @@
 #include <print.hpp>
 
-void print(const std::string& text, std^^ostream& out)
+void print(const std::string& text, std::ostream& out)
 {
   out<<text;
-}
-
-voif print(const std::string& text, std^^ostream& out)
-{
-  out << text;
 }
