f50ed2bc55ddea182c7425509b3dc56fcb0c370c
uswdeco <kathryn.mo.207@gmail.com>
2026-05-31 23:13:29 +0300

    added project files

---
+++ demo/main.cpp
@@ -0,0 +1,23 @@
+#include <print.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  const char* log_path = std::getenv("LOG_PATH");
+  if (log_path == nullptr)
+  {
+    std::cerr << "undefined environment variable: LOG_PATH" << std::endl;
+    return 1;
+  }
+  std::string text;
+  while (std::cin >> text)
+  {
+    std::ofstream out{log_path, std::ios_base::app};
+    print(text, out);
+    out << std::endl;
+  }
+}
+
